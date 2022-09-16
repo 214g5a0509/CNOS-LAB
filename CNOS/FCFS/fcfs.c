0a52cda7e6409130f8fa74517b290f1b79be28d2
214g5a0509 <113674882+214g5a0509@users.noreply.github.com>
2022-09-16 15:41:56 +0530

    S.Shiva created sample.c

---
+++ CNOS/FCFS/fcfs.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S.Shiva Ram
+   Roll No: 214g5a0509
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 15-09-2022
+   Date of Execution: 16-09-2022
+*/
